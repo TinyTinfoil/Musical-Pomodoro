070179103e685677c7dff45224cc3afae08d486a
TinyTinfoil <55459863+TinyTinfoil@users.noreply.github.com>
2021-04-25 20:23:37 -0400

    Add files via upload

---
+++ src/Header.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let version;
+  export let hour, min;
+  let desc = "";
+  $: if (hour === 23) {
+    desc = "Hour of the Rat";
+  } else if (hour === 0) {
+    desc = "Midnight";
+  } else if (hour === 1) {
+    desc = "Hour of the Ox";
+  } else if (hour === 2) {
+    desc = "Late Hour";
+  } else if (hour === 3) {
+    desc = "Hour of the Tiger";
+  } else if (hour === 4) {
+    desc = "Morning Hour";
+  } else if (hour === 5) {
+    desc = "Daybreak";
+  } else {
+    desc = version;
+  }
+</script>
+
+<h1>
+  {hour % 12 || 12}:{min < 10 ? "0" + min : min}
+  {hour > 12 ? "PM" : "AM"}
+  <br />
+  <sub>{desc}</sub>
+</h1>
+<h2>TFH</h2>
+
+<style>
+  h1 {
+    width: fit-content;
+    width: -moz-fit-content;
+    display: inline-block;
+  }
+  sub {
+    font-size: 10px;
+    float: right;
+  }
+  h2 {
+    float: right;
+    margin-right: 5%;
+  }
+</style>
