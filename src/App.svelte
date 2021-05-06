af8973fafc545521321e6d3a6cfc6481618986b6
TinyTinfoil <veeswaranandam@students.natickps.org>
2021-05-06 03:26:07 +0000

    Hopefully made messaging work

--- src/App.svelte
+++ src/App.svelte
@@ -88,7 +88,7 @@
   }
   let plist = false;
   async function messanger(id,message){
-    let resp = await browser.tabs.sendMessage(id,{message:true});
+    let resp = await browser.tabs.sendMessage(id,{[message]:true});
     console.log("Message from the content script:");
     console.log(resp,message);
     return resp;
